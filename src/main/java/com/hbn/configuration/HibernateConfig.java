fc8712b9925ddc948ad4bf5bd0fd5245fb40433d
Ashish Kumar <ashishkumar33176@gmail.com>
2026-07-08 11:51:28 +0530

    update

--- src/main/java/com/hbn/configuration/HibernateConfig.java
+++ src/main/java/com/hbn/configuration/HibernateConfig.java
@@ -1,28 +1,37 @@
 package com.hbn.configuration;
 
+import java.util.Properties;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 
 public class HibernateConfig {
 	
 	
 	
-	
+	 
 	
 	public static SessionFactory getSessionFactory() 
 	
 	{
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		properties.put(Environment.JAKARTA_JDBC_URL, "jdbc:mysql://localhost:3306/mydb");
+		properties.put(Environment.JAKARTA_JDBC_USER, "root");
+		properties.put(Environment.JAKARTA_JDBC_PASSWORD, "root");
+		properties.put(Environment.HBM2DDL_AUTO, "create");
+		properties.put(Environment.SHOW_SQL, "true");
+		properties.put(Environment.FORMAT_SQL, "true");
 		
-		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+		
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings(properties).build();
 		
 		
-		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
-		
-		meta.buildSessionFactory();
+        Metadata meta = new MetadataSources(ssr).addAnnotatedClass(com.hbn.entity.Employee.class).getMetadataBuilder().build();
 		
 		return meta.buildSessionFactory();
 		
