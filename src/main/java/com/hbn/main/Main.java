0c1c722186eed79bfff0840251f6dc63ec2b662f
Ashish Kumar <ashishkumar33176@gmail.com>
2026-07-09 23:14:55 +0530

    commit

--- src/main/java/com/hbn/main/Main.java
+++ src/main/java/com/hbn/main/Main.java
@@ -1,37 +1,58 @@
 package com.hbn.main;
 
-
 import com.hbn.configuration.HibernateConfig;
 import com.hbn.entity.Employee;
+
+import java.util.List;
+
 import org.hibernate.Session;
 
 import org.hibernate.Transaction;
-
+import org.hibernate.query.MutationQuery;
+import org.hibernate.query.Query;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Employee e1 = new Employee("Vinay", "male", 330000);	
-		
-		
-		
+
+		Employee e1 = new Employee("Vinay", "male", 330000);
+
 		Session session = HibernateConfig.getSessionFactory().openSession();
-        Transaction tx = session.beginTransaction();
-        
- //       session.load(e1, 4);
+		Transaction tx = session.beginTransaction();
+
+//        To get all the records from DB        
+//        Query query =   session.createQuery("from Employee",Employee.class);
+//        List list = query.getResultList();   
+//		  System.out.println(list);
+
+//          To get records with Pagination from DB
+//          Query query =   session.createQuery("from Employee",Employee.class); 
+//          query.setFirstResult(2);
+//          query.setMaxResults(5);
+//          List list = query.getResultList();   
+//		    System.out.println(list);
 		
-		Employee employee =session.find(Employee.class, 555);
 		
-//		System.out.println(e1);
+//            To update the records in DB
+//            MutationQuery query = session.createMutationQuery("update Employee set name=:n where id=:i");
+//            query.setParameter("n", "Udit Narayan");
+//            query.setParameter("i", 3);
+//            query.executeUpdate();
+//		      tx.commit();
 		
-		System.out.println(employee);
 		
+//      To delete a records from DB
+//		MutationQuery query = session.createMutationQuery("delete Employee from where id=:i");
+//		query.setParameter("i", 7);
+//		query.executeUpdate();
+		
+		
+//      Aggregation Function
+		Query query1 = session.createQuery("select max(name)from Employee");
+		List list = query1.getResultList();
+		System.out.println(list);
+
 		tx.commit();
-			
-		
-		
-		
 
 	}
 
